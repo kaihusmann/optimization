7df674be29e269b4043876997abee5c4df1d54fa
kaihusmann <khusman@gwdg.de>
2017-10-19 19:02:50 +0200

    vignette

--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(...) {
-  packageStartupMessage("PLEASE NOTE: The package name has changed. The optim_nm and optim_sa functions are now part of the package 'OptimFlex'. The package 'optimization' will be deleted soon.")
-}
